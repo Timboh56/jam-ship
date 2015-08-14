664dde9ade18b6850cd9bae472b2b7479d2b9f19
Tim Hui <timboh56@gmail.com>
2015-08-14 03:05:47 -0700

    add clips production stuff and support for clip upload

--- app/controllers/api/clips_controller.rb
+++ app/controllers/api/clips_controller.rb
@@ -1,6 +1,6 @@
 class Api::ClipsController < ApplicationController
   def create
-    @clip = Clip.create!(user: current_user, channel_id: params[:channel_id], name: params[:fname], attachment: params[:data])
+    @clip = Clip.create!(user: current_user, channel_id: params[:channel_id], name: params[:fname], mp3: params[:data])
     render json: { clip_id: @clip.id, url: @clip.attachment.url }, status: 200
   end
 
