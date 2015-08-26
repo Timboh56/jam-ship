69ea9bd3bed782c8c45813a4d055aa30cd85148c
Tim Hui <timboh56@gmail.com>
2015-08-25 20:18:55 -0700

    clip upload job

--- app/jobs/clip_upload_job.rb
+++ app/jobs/clip_upload_job.rb
@@ -4,8 +4,9 @@ class ClipUploadJob < ActiveJob::Base
   def perform(*args)
     clip = Clip.find(args[0])
     clip.upload_to_s3
-    clip.local_mp3.destroy
+    clip.local_mp3.destroy!
   rescue Exception => e
     Rails.logger.warn e.message
+    p e.message
   end
 end
